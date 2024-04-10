622ce3e11f7704602a854c50df61a5ee583db914
Rai_delgado <alexander.delgado@kushkipagos.com>
2024-04-10 17:55:55 -0500

    Finish implemet API

--- db/mongoDB.go
+++ db/mongoDB.go
@@ -2,8 +2,10 @@ package db
 
 import (
 	"context"
+	"fmt"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"os"
 )
 
 type IConnectMongo interface {
@@ -19,7 +21,8 @@ func NewConnection() IConnectMongo {
 func (c *ConnectionMongo) ConnectMongoDb() (*mongo.Client, error) {
 	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI("mongodb+srv://rai_98:mongo123456@crudcomportamos.9rlbg78.mongodb.net/?retryWrites=true&w=majority&appName=crudComportamos").SetServerAPIOptions(serverAPI)
+	credential := fmt.Sprintf("mongodb+srv://rai_98:%s@crudcomportamos.9rlbg78.mongodb.net/?retryWrites=true&w=majority&appName=crudComportamos", os.Getenv("PSS_DB"))
+	opts := options.Client().ApplyURI(credential).SetServerAPIOptions(serverAPI)
 	// Create a new client and connect to the server
 	return mongo.Connect(context.TODO(), opts)
 }
